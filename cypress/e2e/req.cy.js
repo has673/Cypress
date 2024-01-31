d228682eff8b84e6e732d57fabe12423ade70653
has673 <hassansajjas>
2024-01-31 15:00:20 +0500

    chnages

--- cypress/e2e/req.cy.js
+++ cypress/e2e/req.cy.js
@@ -9,7 +9,7 @@ describe('requests', ()=>{
             expect(response.body).to.have.property('title')
         })
     })
-    it("post",()=>{
+    it.skip("post",()=>{
         cy.request("POST", 'https://jsonplaceholder.typicode.com/posts/' , {
             userId : 1,
             title: 'siu',
@@ -25,6 +25,70 @@ describe('requests', ()=>{
 
         })
     })
+ 
+
+    it.skip('via token',()=>{
+        cy.request({
+            method: "Get",
+            url:"https://gorest.co.in/public/v2/users/6129752",
+            headers:{
+                authorization: "Primary Token    acb1e20f98ca460e1e756fc7c1595a7741091ac716cb87d9f3bbde574c06ebb5 "
+            },
+        }).then((response)=>{
+            expect(response.status).to.eq(200)
+            cy.log(response.body.name)
+
+        })
+    })
+
+    it.skip('post via token',()=>{
+        const uniqueEmail = `W+${Date.now()}@gmail.com`;
+        cy.request({
+            method: "POST",
+            url:"https://gorest.co.in/public/v2/users/",
+            headers:{
+                authorization:" Bearer acb1e20f98ca460e1e756fc7c1595a7741091ac716cb87d9f3bbde574c06ebb5 "
+            },
+            body:{
+               "name" : "ii",
+                "email": uniqueEmail,
+                "gender": "male",
+                "status" : "active"
+            }
+        }).then((response)=>{
+            expect(response.status).to.eq(201)
+            cy.log(response.body.name)
+
+        })
+    })
+
+    
+    it.skip('via token',()=>{
+        cy.request({
+            method: "GET",
+            url:"https://gorest.co.in/public/v2/users/6129952",
+            headers:{
+                authorization: "Primary Token    acb1e20f98ca460e1e756fc7c1595a7741091ac716cb87d9f3bbde574c06ebb5 "
+            },
+        }).then((response)=>{
+            expect(response.status).to.eq(200)
+            cy.log(response.body.name)
+
+        })
+    })
+
+    it.only('delete', () => {
+        cy.request({
+            method: "DELETE",
+            url: "https://gorest.co.in/public/v2/users/6129952",
+            headers: {
+                authorization: "Bearer acb1e20f98ca460e1e756fc7c1595a7741091ac716cb87d9f3bbde574c06ebb5"
+            },
+        }).then((response) => {
+            cy.wrap(response).its('status').should('eq', 204); // Assuming a successful deletion returns status code 204
+            cy.log('done');
+        });
+    });
 
     it.skip("get 1",()=>{
         cy.request('GET','https://jsonplaceholder.typicode.com/posts/5').then((response)=>{
@@ -35,7 +99,7 @@ describe('requests', ()=>{
             
         })
     })
-    it("get mine",()=>{
+    it.skip("get mine",()=>{
         cy.request('GET','https://jsonplaceholder.typicode.com/posts/101').then((response)=>{
             expect(response.status).to.equal(200)
             expect(response.body.title).to.equal("siu")
